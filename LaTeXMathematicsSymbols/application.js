2ba0a02c6266d947afa9ad05ab5c0952fd2d81fa
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-06-01 22:07:08 +0100

    Made the basic structure of the app

---
+++ LaTeXMathematicsSymbols/application.js
@@ -0,0 +1,8 @@
+﻿var application = angular.module("LaTeXMathematicsSymbols", []);
+
+application.controller("SearchController", ["$scope", "$http", function SearchController($scope, $http) {
+    $http.get("symbols.json").then(function (data) {
+        $scope.symbols = data.data;
+    });
+
+}])
