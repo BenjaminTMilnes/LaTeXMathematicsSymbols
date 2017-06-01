83e945635fd9757bc7ccaadabd9003faa550a6f1
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-06-01 22:55:23 +0100

    Added in basic filtering, four basic symbols to the data file, basic styling, and basic layout

--- LaTeXMathematicsSymbols/application.js
+++ LaTeXMathematicsSymbols/application.js
@@ -1,8 +1,35 @@
 ﻿var application = angular.module("LaTeXMathematicsSymbols", []);
 
+application.filter("symbolsNotNull", function () {
+    return function (symbols) {
+        var symbolsNotNull = [];
+
+        for (var i = 0; i < symbols.length; i++) {
+            if (symbols[i].LaTeXCommand != "") {
+                symbolsNotNull.push(symbols[i]);
+            }
+        }
+
+        return symbolsNotNull;
+    };
+});
+
+application.filter("tagsNotNull", function () {
+    return function (tags) {
+        var tagsNotNull = [];
+
+        for (var i = 0; i < tags.length; i++) {
+            if (tags[i] != "") {
+                tagsNotNull.push(tags[i]);
+            }
+        }
+
+        return tagsNotNull;
+    };
+});
+
 application.controller("SearchController", ["$scope", "$http", function SearchController($scope, $http) {
     $http.get("symbols.json").then(function (data) {
         $scope.symbols = data.data;
     });
-
 }])
