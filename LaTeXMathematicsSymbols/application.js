b8e899f76b4d07cb267e9b9edac5835dce91010a
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-06-02 22:46:52 +0100

    Added search function

--- LaTeXMathematicsSymbols/application.js
+++ LaTeXMathematicsSymbols/application.js
@@ -28,6 +28,50 @@ application.filter("tagsNotNull", function () {
     };
 });
 
+function stringContains(string1, string2) {
+    return (string1.indexOf(string2) >= 0);
+}
+
+function stringIsNullOrEmpty(string) {
+    return (!string || /^\s*$/.test(string));
+}
+
+application.filter("searchSymbols", function () {
+    return function (symbols, searchTerms) {
+        console.log("Search Terms: " + searchTerms);
+
+        if (stringIsNullOrEmpty(searchTerms)) {
+            return symbols;
+        }
+        else {
+            var matchingSymbols = [];
+
+            for (var i = 0; i < symbols.length; i++) {
+                var symbol = symbols[i];
+                var titleContainsSearchTerms = stringContains(symbol.Title, searchTerms);
+                var latexContainsSearchTerms = stringContains(symbol.LaTeXCommand, searchTerms);
+                var unicodeContainsSearchTerms = stringContains(symbol.UnicodeCode, searchTerms);
+                var htmlContainsSearchTerms = stringContains(symbol.HTMLEntity, searchTerms);
+                var mathmlContainsSearchTerms = stringContains(symbol.MathMLEntity, searchTerms);
+                var tagsContainSearchTerms = false;
+
+                for (var j = 0; j < symbol.Tags.length; j++) {
+                    var tag = symbol.Tags[j];
+                    if (stringContains(tag, searchTerms)) {
+                        tagsContainSearchTerms = true;
+                    }
+                }
+
+                if (titleContainsSearchTerms || latexContainsSearchTerms || unicodeContainsSearchTerms || htmlContainsSearchTerms || mathmlContainsSearchTerms || tagsContainSearchTerms) {
+                    matchingSymbols.push(symbol);
+                }
+            }
+
+            return matchingSymbols;
+        }
+    }
+});
+
 application.directive("katex", function () {
     return {
         restrict: "E",
